7b38e7bd6b5b8c0fbe2ef1c396ce3438d20be06e
eric.zhang <eric.zhang@pintuibao.cn>
2016-05-16 11:06:27 +0800

    1.上传设计文档

--- package.sh
+++ package.sh
@@ -2,9 +2,11 @@
 rm ./dist/
 HOME=$(cd "$(dirname "$0")/"; pwd)
 cd $HOME
+rm -rf ./dist
 sh uranus-manager/script/release.sh
 sh uranus-server/script/release.sh
 sh uranus-scheduler/script/release.sh
 sh uranus-tools/script/release.sh
 sh uranus-asistant/script/release.sh
+cp -rf doc ./dist/
 
