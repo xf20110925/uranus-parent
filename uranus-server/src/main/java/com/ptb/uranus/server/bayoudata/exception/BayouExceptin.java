5cf8e695ed32a302f7c67547b50042d1cc2122e1
feng.xue <feng.xue@pintuibao.cn>
2016-05-23 19:06:44 +0800

    异常类

---
+++ uranus-server/src/main/java/com/ptb/uranus/server/bayoudata/exception/BayouExceptin.java
@@ -0,0 +1,12 @@
+package com.ptb.uranus.server.bayoudata.exception;
+
+import com.ptb.utils.exception.PTBException;
+
+/**
+ * Created by xuefeng on 2016/5/23.
+ */
+public class BayouExceptin extends PTBException {
+    public BayouExceptin(String message) {
+        super(message);
+    }
+}
