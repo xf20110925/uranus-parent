890d6d5330e4aae943805383f0d0749b17505237
feng.xue <feng.xue@pintuibao.cn>
2016-07-08 21:32:09 +0800

    bug修改

--- uranus-server/src/main/java/com/ptb/uranus/server/third/weixin/BayouWeixinSync.java
+++ uranus-server/src/main/java/com/ptb/uranus/server/third/weixin/BayouWeixinSync.java
@@ -44,8 +44,8 @@ public class BayouWeixinSync {
 
     private void loadConfig() throws ConfigurationException {
         PropertiesConfiguration conf = new PropertiesConfiguration("uranus.properties");
-        RANGEURL = conf.getString("uranus.bayou.range.url", "http://weixindata.pullword.com:58600/%s/range?auth_usr=pintuibao");
-        DATAURL = conf.getString("uranus.bayou.data.url", "http://weixindata.pullword.com:58600/%s/%d?auth_usr=pintuibao");
+        RANGEURL = conf.getString("uranus.bayou.range.url", "http://weixindata.pullword.com:12345/%s/range?auth_usr=pintuibao");
+        DATAURL = conf.getString("uranus.bayou.data.url", "http://weixindata.pullword.com:12345/%s/%d?auth_usr=pintuibao");
     }
 
     public BayouWeixinSync(Sender sender) {
