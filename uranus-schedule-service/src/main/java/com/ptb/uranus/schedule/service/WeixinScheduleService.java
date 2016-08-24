f1f7179332f092a81fda4e26fa24b2f734f05fde
eric <eric.zhang@pintuibao.cn>
2016-08-24 08:55:20 +0800

    [优化微信的调度方式]

--- uranus-schedule-service/src/main/java/com/ptb/uranus/schedule/service/WeixinScheduleService.java
+++ uranus-schedule-service/src/main/java/com/ptb/uranus/schedule/service/WeixinScheduleService.java
@@ -127,9 +127,11 @@ public class WeixinScheduleService {
     }
 
     public void addArticleDynamicScheduler(long postTime, String url) {
-
+        Optional<ScheduleObject> schedulerByField = schedulerDao.getSchedulerByField("obj.conditon", url);
+        if(!schedulerByField.isPresent()) {
         schedulerDao.addCollScheduler(new ScheduleObject<>(new PeriodicTrigger(dynamicFetchInterval, TimeUnit.MINUTES, new Date(postTime * 1000 + dynamicDelayMill), dynamicFetchCount),
                 Priority.L2, new SchedulableCollectCondition(CollectType.C_WX_A_D, url)));
+        };
     }
 
     public void addWeixinDetectNewArticlesScheduleByArticleUrl(String url) {
