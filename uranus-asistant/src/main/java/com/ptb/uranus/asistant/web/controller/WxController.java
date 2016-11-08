ed5330940663d59eedb67dbf4ffd00f478ffe624
feng.xue <feng.xue@pintuibao.cn>
2016-11-08 17:19:11 +0800

    【功能】第三方微信文章动态数据获取

--- uranus-asistant/src/main/java/com/ptb/uranus/asistant/web/controller/WxController.java
+++ uranus-asistant/src/main/java/com/ptb/uranus/asistant/web/controller/WxController.java
@@ -5,6 +5,7 @@ import com.jayway.jsonpath.JsonPath;
 import com.ptb.uranus.asistant.core.entity.JSONResult;
 import com.ptb.uranus.asistant.web.model.rep.RequestUrlResponse;
 import com.ptb.uranus.asistant.web.service.WxService;
+
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,148 +23,158 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class WxController {
-    static Logger logger = LoggerFactory.getLogger(WxController.class);
+	static Logger logger = LoggerFactory.getLogger(WxController.class);
 
 
-    @Resource
-    WxService wxService;
+	@Resource
+	WxService wxService;
 
 
-    @ExceptionHandler({Exception.class})
-    @ResponseBody
-    public String AsisErrorHandler(Exception e) {
-        return JSON.toJSONString(JSONResult.newErrorResult(e.getMessage()));
-    }
+	@ExceptionHandler({Exception.class})
+	@ResponseBody
+	public String AsisErrorHandler(Exception e) {
+		return JSON.toJSONString(JSONResult.newErrorResult(e.getMessage()));
+	}
 
-    @RequestMapping(value = "wx/mobile/getNewUrl")
-    @ResponseBody
-    public String getNewUrl(HttpServletRequest request, @RequestParam("callback") String callback,
-                            @RequestParam("data") String data) {
-        try {
-            JSONResult jsonResult = new JSONResult();
+	@RequestMapping(value = "wx/mobile/getNewUrl")
+	@ResponseBody
+	public String getNewUrl(HttpServletRequest request, @RequestParam("callback") String callback,
+	                        @RequestParam("data") String data) {
+		try {
+			JSONResult jsonResult = new JSONResult();
 
-            if (data != null) {
-                String url = JsonPath.parse(data).read("$.url");
-                if (url.contains("key=") && url.contains("qq")) {
-                    wxService.updateRealUrl(url);
-                    wxService.updateRealUrlV1(url);
-                    if (url.contains("mp.weixin.qq.com/s")) {
-                        wxService.updateRealUrlAndReadNum(data);
-                    }
-                }
-            }
+			if (data != null) {
+				String url = JsonPath.parse(data).read("$.url");
+				if (url.contains("key=") && url.contains("qq")) {
+					wxService.updateRealUrl(url);
+					wxService.updateRealUrlV1(url);
+					if (url.contains("mp.weixin.qq.com/s")) {
+						wxService.updateRealUrlAndReadNum(data);
+					}
+				}
+			}
 
-            String url = wxService.getNextRedirectUrl();
-            String result;
+			String url = wxService.getNextRedirectUrl();
+			String result;
 
-            if (url == null) {
-                result = JSONResult.newErrorResult(-1, "null task").toJson();
-            } else {
-                RequestUrlResponse requestUrlResponse = new RequestUrlResponse(url.replaceFirst("&key=[^&]*]", "").
-                        replaceFirst("&pass_ticket=[^&]*]", "").
-                        replaceFirst("&wechat_redirect", "").replaceFirst("#rd", "") + "#rd&wechat_redirect");
-                result = jsonResult.setData(requestUrlResponse).toJson();
-            }
+			if (url == null) {
+				result = JSONResult.newErrorResult(-1, "null task").toJson();
+			} else {
+				RequestUrlResponse requestUrlResponse = new RequestUrlResponse(url.replaceFirst("&key=[^&]*]", "").
+					replaceFirst("&pass_ticket=[^&]*]", "").
+					replaceFirst("&wechat_redirect", "").replaceFirst("#rd", "") + "#rd&wechat_redirect");
+				result = jsonResult.setData(requestUrlResponse).toJson();
+			}
 
-            return String.format("%s(%s)", callback, result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return String.format("%s(%s)", callback, JSONResult.newErrorResult(-1, e.getMessage()).toJson());
-        }
-    }
-
-
-    @RequestMapping(value = "wx/getVaildUrl")
-    @ResponseBody
-    public String getVaildUrl(HttpServletRequest request, @RequestParam("url") String url) {
-        try {
-            if (url != null) {
-                return new JSONResult(wxService.getRedirectUrl(url)).toJson();
-            } else {
-                return JSONResult.newErrorResult(-1, "无效有URL地址").toJson();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
-        }
-    }
-
-    @RequestMapping(value = "wx/add")
-    @ResponseBody
-    public String wxTaskAdd(@RequestParam("url") String url) {
-        try {
-            if (url != null) {
-                wxService.addWxUrlToQueue(url);
-                return new JSONResult<>().toJson();
-            } else {
-                return JSONResult.newErrorResult(-1, "无效有URL地址").toJson();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
-        }
-    }
-
-    @RequestMapping(value = "wx/get")
-    @ResponseBody
-    public String resultGet(@RequestParam("key") String key) {
-        try {
-            if (key != null) {
-                String result = wxService.getValueByKey(key);
-                if(result == null) result = "";
-                return new JSONResult<String>(result).toJson();
-            } else {
-                return JSONResult.newErrorResult(-1, "无效有URL地址").toJson();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
-        }
-    }
-
-    @RequestMapping(value = "wx/update")
-    @ResponseBody
-    public String resultUpdate( @RequestParam("data") String data) {
-        try {
-            if (data !=null) {
-                wxService.updateRealUrlAndReadNum(data);
-                return new JSONResult<>().toJson();
-            }else{
-                return JSONResult.newErrorResult(-1, "无效的数据").toJson();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
-        }
-    }
-
-    @RequestMapping(value = "wx/mapLinkAdd")
-    @ResponseBody
-    public String mapLinkAdd( @RequestParam("realUrl") String realUrl, @RequestParam("sogouUrl") String sogouUrl) {
-        try {
-            if (StringUtils.isNotBlank(realUrl) && StringUtils.isNotBlank(sogouUrl)) {
-                wxService.mapLinkAdd(realUrl, sogouUrl);
-                return new JSONResult<>().toJson();
-            } else {
-                return JSONResult.newErrorResult(-1, "无效的数据").toJson();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
-        }
-    }
+			return String.format("%s(%s)", callback, result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return String.format("%s(%s)", callback, JSONResult.newErrorResult(-1, e.getMessage()).toJson());
+		}
+	}
 
 
-    @RequestMapping(value = "wx/mapLinkGet")
-    @ResponseBody
-    public String mapLinkGet( @RequestParam("key") String key) {
-        try {
-            if (StringUtils.isNotBlank(key)) {
-                return wxService.mapLinkGet(key);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	@RequestMapping(value = "wx/getVaildUrl")
+	@ResponseBody
+	public String getVaildUrl(HttpServletRequest request, @RequestParam("url") String url) {
+		try {
+			if (url != null) {
+				return new JSONResult(wxService.getRedirectUrl(url)).toJson();
+			} else {
+				return JSONResult.newErrorResult(-1, "无效有URL地址").toJson();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
+		}
+	}
+
+	@RequestMapping(value = "wx/add")
+	@ResponseBody
+	public String wxTaskAdd(@RequestParam("url") String url) {
+		try {
+			if (url != null) {
+				wxService.addWxUrlToQueue(url);
+				return new JSONResult<>().toJson();
+			} else {
+				return JSONResult.newErrorResult(-1, "无效有URL地址").toJson();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
+		}
+	}
+
+	@RequestMapping(value = "wx/get")
+	@ResponseBody
+	public String resultGet(@RequestParam("key") String key) {
+		try {
+			if (key != null) {
+				String result = wxService.getValueByKey(key);
+				if (result == null) result = "";
+				return new JSONResult<String>(result).toJson();
+			} else {
+				return JSONResult.newErrorResult(-1, "无效有URL地址").toJson();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
+		}
+	}
+
+	@RequestMapping(value = "wx/update")
+	@ResponseBody
+	public String resultUpdate(@RequestParam("data") String data) {
+		try {
+			if (data != null) {
+				wxService.updateRealUrlAndReadNum(data);
+				return new JSONResult<>().toJson();
+			} else {
+				return JSONResult.newErrorResult(-1, "无效的数据").toJson();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
+		}
+	}
+
+	@RequestMapping(value = "wx/mapLinkAdd")
+	@ResponseBody
+	public String mapLinkAdd(@RequestParam("realUrl") String realUrl, @RequestParam("sogouUrl") String sogouUrl) {
+		try {
+			if (StringUtils.isNotBlank(realUrl) && StringUtils.isNotBlank(sogouUrl)) {
+				wxService.mapLinkAdd(realUrl, sogouUrl);
+				return new JSONResult<>().toJson();
+			} else {
+				return JSONResult.newErrorResult(-1, "无效的数据").toJson();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return JSONResult.newErrorResult(-1, e.getMessage()).toJson();
+		}
+	}
+
+
+	@RequestMapping(value = "wx/mapLinkGet")
+	@ResponseBody
+	public String mapLinkGet(@RequestParam("key") String key) {
+		try {
+			if (StringUtils.isNotBlank(key)) {
+				return wxService.mapLinkGet(key);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	@RequestMapping(value = "wx/readLike")
+	@ResponseBody
+	public String getReadLikeByBaYou(@RequestParam("url") String url) {
+		if (StringUtils.isNotBlank(url)) {
+			return wxService.getReadLikeByBaYou(url);
+		}
+		return null;
+	}
 }
--- uranus-asistant/src/main/java/com/ptb/uranus/asistant/web/service/WxService.java
+++ uranus-asistant/src/main/java/com/ptb/uranus/asistant/web/service/WxService.java
@@ -6,8 +6,15 @@ import com.jayway.jsonpath.JsonPath;
 import com.ptb.uranus.asistant.core.entity.ReadLikeNum;
 import com.ptb.uranus.asistant.core.util.RedisUtil;
 import com.ptb.uranus.asistant.core.util.WxUtils;
+import com.ptb.uranus.spider.weixin.parse.BayouWeixinParser;
 import com.ptb.utils.exception.PTBException;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.locks.ReentrantLock;
+
 import redis.clients.jedis.Jedis;
 
 /**
@@ -16,127 +23,152 @@ import redis.clients.jedis.Jedis;
 @Service
 public class WxService {
 
-    String Q_WX_redirect = "Q_WX_redirect";
+	String Q_WX_redirect = "Q_WX_redirect";
 
-    int waitTimeout = 60;
-    int resultTimeout = 120;
-    int retryInterval = 20;
-    int keyV2ExpireSecond = 3600;
-    int mapLinkTimeOut = 2*24*60*60;
-
-    public String getRedirectUrl(String urlStr) {
-        Jedis jedis = RedisUtil.getJedis();
-        try {
-            String resKey = "";
-
-            resKey = WxUtils.getUrlKey(urlStr);
-            int i = 0;
-            while (i < waitTimeout) {
-                String hget = jedis.get(resKey);
-                if (hget != null && hget.length() > 0) {
-                    return hget;
-                }
-                if (i % retryInterval == 0) {
-                    jedis.lpush(Q_WX_redirect, urlStr);
-                }
-                i++;
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            throw new PTBException(String.format("等待有效KEY,超时,url %s", urlStr));
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-
-    }
-
-    public String getNextRedirectUrl() {
-        Jedis jedis = RedisUtil.getJedis();
-        try {
-            String url = jedis.lpop(Q_WX_redirect);
-            if (url == null) {
-                return null;
-            }
-
-            String urlKey = WxUtils.getUrlKey(url);
-            String hget = jedis.get(urlKey);
-            if (hget != null && hget.length() > 10) {
-                return null;
-            }
-            return url;
-
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
-
-    public void updateRealUrl(String url) {
-        Jedis jedis = RedisUtil.getJedis();
-        try {
-            jedis.setex(WxUtils.getUrlKey(url), resultTimeout, url);
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
+	int waitTimeout = 60;
+	int resultTimeout = 120;
+	int retryInterval = 20;
+	int keyV2ExpireSecond = 3600;
+	int mapLinkTimeOut = 2 * 24 * 60 * 60;
 
 
-    public void addWxUrlToQueue(String url) {
-        Jedis jedis = RedisUtil.getJedis();
-        try {
-            jedis.lpush(Q_WX_redirect, url);
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
+	public String getRedirectUrl(String urlStr) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			String resKey = "";
 
-    public String getValueByKey(String key) {
-        Jedis jedis = RedisUtil.getJedis();
-        try {
-            return jedis.get(key);
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
+			resKey = WxUtils.getUrlKey(urlStr);
+			int i = 0;
+			while (i < waitTimeout) {
+				String hget = jedis.get(resKey);
+				if (hget != null && hget.length() > 0) {
+					return hget;
+				}
+				if (i % retryInterval == 0) {
+					jedis.lpush(Q_WX_redirect, urlStr);
+				}
+				i++;
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			throw new PTBException(String.format("等待有效KEY,超时,url %s", urlStr));
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
 
-    public void updateRealUrlAndReadNum(String data) {
-        Jedis jedis = RedisUtil.getJedis();
-        try {
-            DocumentContext parse = JsonPath.parse(data);
-            String url = parse.read("$.url").toString();
-            String readNum = parse.read("$.readNum").toString();
-            String likeNum = parse.read("$.likeNum").toString();
-            jedis.setex(WxUtils.getUrlKeyV1(url), resultTimeout, JSON.toJSONString(new ReadLikeNum(readNum, likeNum)));
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
+	}
 
-    public void updateRealUrlV1(String url) {
-        Jedis jedis = RedisUtil.getJedis();
-        try {
-            jedis.setex(WxUtils.getUrlKeyV2(url), keyV2ExpireSecond, url);
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
+	public String getNextRedirectUrl() {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			String url = jedis.lpop(Q_WX_redirect);
+			if (url == null) {
+				return null;
+			}
 
-    public void mapLinkAdd(String realUrl, String sogouUrl){
-         Jedis jedis = RedisUtil.getJedis();
-        try {
-                jedis.setex(realUrl,mapLinkTimeOut, sogouUrl);
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
-    public String mapLinkGet(String key){
-         Jedis jedis = RedisUtil.getJedis();
-        try {
-               return jedis.get(key);
-        } finally {
-            RedisUtil.returnResource(jedis);
-        }
-    }
+			String urlKey = WxUtils.getUrlKey(url);
+			String hget = jedis.get(urlKey);
+			if (hget != null && hget.length() > 10) {
+				return null;
+			}
+			return url;
+
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+	public void updateRealUrl(String url) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			jedis.setex(WxUtils.getUrlKey(url), resultTimeout, url);
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+
+	public void addWxUrlToQueue(String url) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			jedis.lpush(Q_WX_redirect, url);
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+	public String getValueByKey(String key) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			return jedis.get(key);
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+	public void updateRealUrlAndReadNum(String data) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			DocumentContext parse = JsonPath.parse(data);
+			String url = parse.read("$.url").toString();
+			String readNum = parse.read("$.readNum").toString();
+			String likeNum = parse.read("$.likeNum").toString();
+			jedis.setex(WxUtils.getUrlKeyV1(url), resultTimeout, JSON.toJSONString(new ReadLikeNum(readNum, likeNum)));
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+	public void updateRealUrlV1(String url) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			jedis.setex(WxUtils.getUrlKeyV2(url), keyV2ExpireSecond, url);
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+	public void mapLinkAdd(String realUrl, String sogouUrl) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			jedis.setex(realUrl, mapLinkTimeOut, sogouUrl);
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+	public String mapLinkGet(String key) {
+		Jedis jedis = RedisUtil.getJedis();
+		try {
+			return jedis.get(key);
+		} finally {
+			RedisUtil.returnResource(jedis);
+		}
+	}
+
+	//控制访问频率锁
+	private static ReentrantLock lock = new ReentrantLock();
+	private BayouWeixinParser parser = new BayouWeixinParser();
+	static Logger requestLogger = Logger.getLogger("bayou.request");
+
+	public String getReadLikeByBaYou(String url) {
+		try {
+			lock.lock();
+			Thread.sleep(500);
+			Optional<com.ptb.uranus.spider.weixin.bean.ReadLikeNum> readLikeOpt = parser.getReadLikeNumByArticleUrl(url);
+			if (readLikeOpt.isPresent()) {
+				requestLogger.error(String.format("request success url-> %s, time->%s", url, System.currentTimeMillis()/1000));
+				return readLikeOpt.get().toString();
+			}
+			requestLogger.error(String.format("request failed url->%s, time->%s", url, System.currentTimeMillis()/1000));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+		return "";
+	}
 }
