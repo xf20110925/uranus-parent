53837e675c8bb13a6c658de803fb02426d0885c6
feng.xue <feng.xue@pintuibao.cn>
2016-11-15 20:10:39 +0800

    【bug】uranus-schedule数据库mongo迁移

--- uranus-schedule-service/src/main/java/com/ptb/uranus/schedule/utils/MongoUtils.java
+++ uranus-schedule-service/src/main/java/com/ptb/uranus/schedule/utils/MongoUtils.java
@@ -5,22 +5,61 @@
 
 package com.ptb.uranus.schedule.utils;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
+import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.lang.StringUtils;
 import org.bson.Document;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public enum MongoUtils {
 	instance;
+	private List<String> hosts;
+	private int port;
+	private MongoClient mongoClient;
 
 	MongoUtils() {
+		try {
+			Configuration conf = new PropertiesConfiguration("ptb.properties");
+			String mongoHosts = conf.getString("uranus.scheduler.mongo.host");
+			if (mongoHosts.contains(",")){
+				this.hosts = Arrays.asList(mongoHosts.split(","));
+			}else if(mongoHosts.contains("|")){
+				this.hosts = Arrays.asList(mongoHosts.split("\\|"));
+			}else{
+				this.hosts = Arrays.asList(mongoHosts);
+			}
+			this.port = conf.getInt("uranus.scheduler.mongo.port");
+			this.mongoClient = getMongoClientV2(hosts, port);
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static MongoClient getMongoClientV2(List<String>  host, int port) {
+		ReadPreference readPreference = ReadPreference.secondaryPreferred();
+		List<ServerAddress> addresses = host.stream().map(x -> new ServerAddress(x, port)).collect(Collectors.toList());
+		MongoClientOptions.Builder options = new MongoClientOptions.Builder().readPreference(readPreference)
+			.connectionsPerHost(300).connectTimeout(30000).maxWaitTime(5000).socketTimeout(0)
+			.threadsAllowedToBlockForConnectionMultiplier(5000).writeConcern(WriteConcern.ACKNOWLEDGED);
+		return new MongoClient(addresses, options.build());
 	}
 
 	public MongoDatabase getDatabase(String dbName) {
 		try {
-			if (dbName != null && !"".equals(dbName)) {
-				MongoDatabase database = com.ptb.utils.gaiatools.MongoUtils.instance().getDatabase(dbName);
-				return database;
+			if (StringUtils.isNotBlank(dbName)) {
+				return instance.mongoClient.getDatabase(dbName);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -44,5 +83,9 @@ public enum MongoUtils {
 
 	public void dropCollection(String dbName, String collName) {
 		getCollection(dbName, collName).drop();
+	}
+
+	public static void main(String[] args) {
+		MongoDatabase test = MongoUtils.instance.getDatabase("test");
 	}
 }
