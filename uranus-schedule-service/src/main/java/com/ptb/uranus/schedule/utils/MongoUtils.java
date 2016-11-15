92750e545c1b9dc5358f58a460d781060740f641
feng.xue <feng.xue@pintuibao.cn>
2016-11-15 17:37:56 +0800

    【bug】uranus-schedule数据库mongo迁移

--- uranus-schedule-service/src/main/java/com/ptb/uranus/schedule/utils/MongoUtils.java
+++ uranus-schedule-service/src/main/java/com/ptb/uranus/schedule/utils/MongoUtils.java
@@ -11,35 +11,38 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 public enum MongoUtils {
-    instance;
+	instance;
 
-    MongoUtils() {
-    }
+	MongoUtils() {
+	}
 
-    public MongoDatabase getDatabase(String dbName) {
-        try {
-            if (dbName != null && !"".equals(dbName)) {
-                MongoDatabase database = com.ptb.utils.gaiatools.MongoUtils.instance().getDatabase(dbName);
-                return database;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        throw new IllegalArgumentException("连接mongo失败！");
-    }
+	public MongoDatabase getDatabase(String dbName) {
+		try {
+			if (dbName != null && !"".equals(dbName)) {
+				MongoDatabase database = com.ptb.utils.gaiatools.MongoUtils.instance().getDatabase(dbName);
+				return database;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		throw new IllegalArgumentException("连接mongo失败！");
+	}
 
 
-    public MongoCollection<Document> getCollection(String dbName, String collName) {
-        if (null != collName && !"".equals(collName)) {
-            if (null != dbName && !"".equals(dbName)) {
-                MongoCollection collection = this.getDatabase(dbName).getCollection(collName);
-                return collection;
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
+	public MongoCollection<Document> getCollection(String dbName, String collName) {
+		if (null != collName && !"".equals(collName)) {
+			if (null != dbName && !"".equals(dbName)) {
+				MongoCollection collection = this.getDatabase(dbName).getCollection(collName);
+				return collection;
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
 
+	public void dropCollection(String dbName, String collName) {
+		getCollection(dbName, collName).drop();
+	}
 }
