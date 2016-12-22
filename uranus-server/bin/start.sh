6dc7991fad7adc102b47a8f4e9429d074b38b3fb
feng.xue <feng.xue@pintuibao.cn>
2016-12-22 15:31:47 +0800

    【环境】调度修改

--- uranus-server/bin/start.sh
+++ uranus-server/bin/start.sh
@@ -3,13 +3,12 @@
 
 source /etc/profile
 URANUS_HOME=$(cd "$(dirname "$0")/../"; pwd)
-
+CONFIG_FILE_HOME=/opt/ptbconf/uranus-server
 cd $URANUS_HOME
 
 #debug_model="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=9999"
 main_class="com.ptb.uranus.server.Entry"
 lib_dir="libs/"
-config_dir="config/"
 
 allclasspath=`ls $lib_dir | grep .jar | awk '{jar=jar"'"$lib_dir"'"$1":"} END {print jar}'`
 
@@ -20,28 +19,28 @@ TOPIC=uranus-server_v2_wx_s
 if [ ! -d logs/${TOPIC} ]; then
     mkdir logs/${TOPIC}
 fi
-nohup java -cp $config_dir:$allclasspath$jars -Dtopic=${TOPIC} ${main_class} \
+java -cp $CONFIG_FILE_HOME:$allclasspath$jars -Dtopic=${TOPIC} ${main_class} \
 --listenTopics=${TOPIC} \
 --spider.worker.num=5 \
---kafka.consumer.num=1 >> logs/${TOPIC}/run.log 2>&1 &
+--kafka.consumer.num=1 >> logs/${TOPIC}/run.log 2>&1
 
 TOPIC=uranus-server_v2_wx_d
 if [ ! -d logs/${TOPIC} ]; then
     mkdir logs/${TOPIC}
 fi
-nohup java -cp $config_dir:$allclasspath$jars -Dtopic=${TOPIC} ${main_class} \
+java -cp $CONFIG_FILE_HOME:$allclasspath$jars -Dtopic=${TOPIC} ${main_class} \
 --listenTopics=${TOPIC} \
 --spider.worker.num=3 \
---kafka.consumer.num=1 >> logs/${TOPIC}/run.log 2>&1 &
+--kafka.consumer.num=1 >> logs/${TOPIC}/run.log 2>&1
 
 TOPIC=uranus-server_v2_wb
 if [ ! -d logs/${TOPIC} ]; then
     mkdir logs/${TOPIC}
 fi
-nohup java -cp $config_dir:$allclasspath$jars -Dtopic=${TOPIC} ${main_class} \
+java -cp $CONFIG_FILE_HOME:$allclasspath$jars -Dtopic=${TOPIC} ${main_class} \
 --listenTopics=${TOPIC} \
---spider.worker.num=4 \
---kafka.consumer.num=1 >> logs/${TOPIC}/run.log  2>&1 &
+--spider.worker.num=8 \
+--kafka.consumer.num=1 >> logs/${TOPIC}/run.log  2>&1
 
 #TOPIC=uranus-server_v2
 #if [ ! -d logs/${TOPIC} ]; then
--- uranus-server/bin/third_data_start.sh
+++ uranus-server/bin/third_data_start.sh
@@ -1,10 +1,11 @@
  #!/usr/bin/env bash
  source /etc/profile
  HOME=$(cd "$(dirname "$0")/../"; pwd)
+ CONFIG_FILE_HOME=/opt/ptbconf/uranus-server
  cd $HOME
  jars=$(ls $HOME/|grep .jar)
  libjar=`ls libs | grep .jar | awk '{jar=jar"'"libs/"'"$1":"} END {print jar}'`
  echo $jars
 
- echo $HOME/config:$jars:$libjar
- nohup java -cp $HOME/config:$libjar:$jars  -Dtopic=third com.ptb.uranus.server.third.version2.ThirdEntryV2 $* >>$HOME/logs/run.log 2>&1 &
+ echo $CONFIG_FILE_HOME:$jars:$libjar
+ java -cp $CONFIG_FILE_HOME:$libjar:$jars  -Dtopic=third com.ptb.uranus.server.third.version2.ThirdEntryV2 $* >>$HOME/logs/run.log 2>&1
